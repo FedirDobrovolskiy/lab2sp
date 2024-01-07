439d4e485453740a30c9dad0f4c9cee9a3171020
Fedir Dobrovolskyi <76664060+FedirDobrovolskiy@users.noreply.github.com>
2024-01-07 15:17:19 +0200

    Add files via upload

---
+++ calculator.h
@@ -0,0 +1,21 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator
+{
+<<<<<<< HEAD
+    public:
+
+        int Add (double, double);
+        int Sub (double, double);
+        int Mul (double, double);
+        double Dilennia (double, double);
+
+=======
+	public:
+		int Add (double, double);
+		int Sub (double, double);
+>>>>>>> 4ad40a3b4572deee4efc716e7bd6e6e4fda46dcf
+};
+
+#endif//CALCULATOR_H
